34b45a0bfff45390c2b112e533aae77b4f621c11
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-04-02 22:59:44 +0800

    Modify the structure of CLI

--- ynmt_cli/generate.py
+++ ynmt_cli/generate.py
@@ -9,14 +9,8 @@
 # This source code is licensed under the Apache-2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 
-import cupy
-
-def cli_main():
-    parser = options.get_generation_parser(default_task='document_translation')
-    youngseq_options.add_document_generation_args(parser)
-    args = options.parse_args_and_arch(parser)
-    main(args)
+from ynmt.binaries.generate import main
 
 
 if __name__ == '__main__':
-    cli_main()
+    main()
