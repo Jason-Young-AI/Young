59cb4e7a2f6145abb45465c05a34f75b5b299551
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-04-02 23:01:17 +0800

    Add new binary files

---
+++ ynmt/binaries/train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-04-02 08:23
+#
+# This source code is licensed under the Apache-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+import cupy
+
+def cli_main():
+    parser = options.get_generation_parser(default_task='document_translation')
+    youngseq_options.add_document_generation_args(parser)
+    args = options.parse_args_and_arch(parser)
+    main(args)
+
+
+if __name__ == '__main__':
+    cli_main()
