efe842d947218cc6b8d3d7d52efb7ef3aa8f474b
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-04-02 23:02:23 +0800

    Fix several bugs; Add update_value() and update() method.

---
+++ ynmt/hocons/arguments.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-04-01 20:09
+#
+# This source code is licensed under the Apache-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import pyhocon
+
+
+DEFAULT_HOCON_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class HOCONArguments(object):
+    def __init__(self, hocon, name):
+        if isinstance(hocon, pyhocon.config_tree.ConfigTree):
+            if name in hocon:
+                self.__name = name
+                self.__succeed_names = []
+                if isinstance(hocon[name], pyhocon.config_tree.ConfigTree):
+                    self.__value = None
+                    for succeed_name in hocon[name]:
+                        self.__succeed_names.append(succeed_name)
+                        setattr(self, succeed_name, HOCONArguments(hocon[name], succeed_name))
+                else:
+                    self.__value = hocon[name]
+            else:
+                raise ValueError(f'Hocon(\'{hocon}\') does not have name(\'{name}\').')
+        else:
+            raise ValueError(f'Argument hocon(\'{hocon}\') is not a \'pyhocon.config_tree.ConfigTree\' object.')
+
+    def __setattr__(self, key, value):
+        if key in ['_HOCONArguments__name', '_HOCONArguments__succeed_names']:
+            if key in self.__dict__:
+                raise AttributeError(f'Can not modify Reserved Attribute \'{key}\'.')
+            else:
+                self.__dict__[key] = value
+        elif key in self.__succeed_names:
+            if key in self.__dict__:
+                raise AttributeError(f'Can not rewrite HOCONArguments object attribute \'{key}\'.')
+            else:
+                self.__dict__[key] = value
+        elif key in ['_HOCONArguments__value',]:
+            self.__dict__[key] = value
+        else:
+            raise AttributeError(f'This Attribute: \'{key}\' does not exist.')
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def value(self):
+        return self.__value
+
+    def update_value(self, new_value):
+        self.__value == new_value
+
+    def update(self, hocon, name):
+        if name == self.name:
+            if isinstance(hocon, pyhocon.config_tree.ConfigTree):
+                if name in hocon:
+                    if isinstance(hocon[name], pyhocon.config_tree.ConfigTree):
+                        for succeed_name in hocon[name]:
+                            if succeed_name in self.__succeed_names:
+                                succeed = getattr(self, succeed_name)
+                                succeed.update(hocon[name], succeed_name)
+                            else:
+                                self.__succeed_names.append(succeed_name)
+                                setattr(self, succeed_name, HOCONArguments(hocon[name], succeed_name))
+                    else:
+                        self.update_value(hocon[name])
+                else:
+                    raise ValueError(f'Hocon(\'{hocon}\') does not have name(\'{name}\').')
+            else:
+                raise ValueError(f'Argument hocon(\'{hocon}\') is not a \'pyhocon.config_tree.ConfigTree\' object.')
+        else:
+            raise ValueError(f'Argument name(\'{name}\') must match obj.name(\'{self.name}\').')
+
+
+def get_train_arguments(hocon_filepath=None):
+    pass
+
+
+def load_default_arguments():
+    default_hocon = pyhocon.ConfigFactory.parse_file(os.path.join(DEFAULT_HOCON_DIR, 'default.hocon'))
+    default_binary_arguments = HOCONArguments(default_hocon, 'binary')
+    default_model_arguments = HOCONArguments(default_hocon, 'model')
+    return {
+        'binary': default_binary_arguments,
+        'model': default_model_arguments,
+    }
