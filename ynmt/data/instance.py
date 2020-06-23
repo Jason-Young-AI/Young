c7162db27bd74eee325464be037b821c82787e06
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-06-23 17:15:03 +0800

    Add InstanceFilter and InstanceSizeCalculator, Update Instance.

--- ynmt/data/instance.py
+++ ynmt/data/instance.py
@@ -10,6 +10,36 @@
 # LICENSE file in the root directory of this source tree.
 
 
+def InstanceFilter(object):
+    def __init__(self, length_intervals):
+        self.length_intervals = length_intervals
+
+    def __call__(self, instance):
+        for attribute_name, length_interval in self.length_intervals.items():
+            assert attribute_name in instance.structure, f"No such attribute:{attribute_name} in instance:{instance}"
+            attribute_length = len(instance[attribute_name])
+            if length_interval[0] < attribute_length and attribute_length < length_interval[1]:
+                return False
+            else:
+                return True
+
+
+def InstanceSizeCalculator(object):
+    def __init__(self, calculate_attribute, calculate_type):
+        assert calculate_type in {'token', 'sentence'}, "Wrong choice of calculator type."
+
+        self.calculate_attribute = calculate_attribute
+        self.calculate_type = calculate_type
+
+    def __call__(self, instance):
+        if self.calculate_type == 'sentence':
+            return 1
+
+        if self.calculate_type == 'token':
+            attribute = instance[self.calculate_attribute]
+            return len(attribute)
+
+
 class Instance(object):
     def __init__(self, structure):
         assert isinstance(structure, set), 'Type of structure should be set().'
@@ -22,6 +52,12 @@ class Instance(object):
 
     def __len__(self):
         return len(self.__structure)
+
+    def __setitem__(self, attribute_name, attribute_value):
+        self.__dict__[attribute_name] = attribute_value
+
+    def __getitem__(self, attribute_name):
+        return self.__dict__[attribute_name]
 
     def __iter__(self):
         for attribute_name in self.structure:
