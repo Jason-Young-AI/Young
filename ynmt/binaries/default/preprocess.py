1223bc9a7f5fa65fb552f9a568decc10f640600a
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-06-23 17:13:25 +0800

    Update preprocess method

--- ynmt/binaries/default/preprocess.py
+++ ynmt/binaries/default/preprocess.py
@@ -57,52 +57,57 @@ def get_token_counter(file_path, number_worker):
     return total_token_counter
 
 
-def build_vocabulary(args, source_path, target_path, number_worker, logger):
-    source_token_counter = get_token_counter(source_path, number_worker)
-    target_token_counter = get_token_counter(target_path, number_worker)
-    if args.vocabulary.share:
-        logger.info(' * Share Vocabulary')
+def build_vocabularies(sides, groups, sizes, paths, languages, number_worker, logger):
+    token_counters = dict()
+    for index, side in enumerate(sides):
+        logger.info(f' * No.{index} - {side}')
+        path = getattr(paths, side)
+        logger.info(f'   corpus: {path}')
+        language = getattr(languages, side)
+        logger.info(f'   language: {language}')
+        token_counter = get_token_counter(path, number_worker)
+        logger.info(f'   {len(token_counter)} token found')
+        token_counters[side] = token_counter
+
+    vocabularies = dict()
+    for group, size in zip(groups, sizes):
+        logger.info(f' * Sides {group} will be merged with size limit {size}')
         token_counter = collections.Counter()
-        token_counter.update(source_token_counter)
-        token_counter.update(target_token_counter)
-        vocabulary = Vocabulary(list(token_counter.items()), args.vocabulary.size)
-        source_vocabulary = vocabulary
-        target_vocabulary = vocabulary
-    else:
-        source_vocabulary = Vocabulary(list(source_token_counter.items()), args.vocabulary.source_size)
-        target_vocabulary = Vocabulary(list(target_token_counter.items()), args.vocabulary.target_size)
+        for side in group:
+            token_counter.update(token_counters[side])
+        vocabulary = Vocabulary(list(token_counter.items()), size)
+        for side in group:
+            vocabularies[side] = vocabulary
+        logger.info(f'   Merged vocabulary size is {len(vocabulary)}')
 
-    logger.info(f' * source vocabulary {len(source_vocabulary)} token')
-    logger.info(f' * target vocabulary {len(target_vocabulary)} token')
+    return vocabularies
 
-    return source_vocabulary, target_vocabulary
 
-def build_instances(source_path, target_path,
-                    source_edge, target_edge,
-                    source_vocabulary, target_vocabulary,
-                    structure):
+def build_instances(primary_side, sides, paths, vocabularies, corpora_edge):
+    structure = set(sides)
+    files = dict()
+    for side in sides:
+        corpus_edge_start, corpus_edge_end = corpora_edge[side]
+        files[side]= open(getattr(paths, side), 'r', encoding='utf-8')
+        files[side].seek(corpus_edge_start)
+
     instances = list()
-    source_edge_start, source_edge_end = source_edge
-    target_edge_start, target_edge_end = target_edge
-    with open(source_path, 'r', encoding='utf-8') as source_file, open(target_path, 'r', encoding='utf-8') as target_file:
-        source_file.seek(source_edge_start)
-        target_file.seek(target_edge_start)
-        while source_file.tell() < source_edge_end:
-            source_line = source_file.readline()
-            target_line = target_file.readline()
-            instance = Instance(structure)
-            instance.source = numericalize(source_line, source_vocabulary)
-            instance.target = numericalize(target_line, target_vocabulary)
-            instances.append(instance)
+    primary_corpus_edge_start, primary_corpus_edge_end = corpora_edge[primary_side]
+    while files[primary_side].tell() < primary_corpus_edge_end:
+        instance = Instance(structure)
+        for side in sides:
+            instance[side] = numericalize(files[side].readline(), vocabularies[side])
+        instances.append(instance)
+
+    for side in sides:
+        files[side].close()
+
     return instances
 
 
-def get_partial_dataset(source_path, target_path,
-                        source_vocabulary, target_vocabulary,
-                        source_edges, target_edges,
-                        edge_index_start, edge_index_end,
-                        number_worker):
-    structure = set({'source', 'target'})
+def get_partial_dataset(primary_side, sides, paths, vocabularies, corpora_edges,
+                        edge_index_start, edge_index_end, number_worker):
+    structure = set(sides)
     partial_dataset = Dataset(structure)
 
     def add_instances(instances):
@@ -112,92 +117,100 @@ def get_partial_dataset(source_path, target_path,
     with multiprocessing.Pool(number_worker) as pool:
         results = list()
         for edge_index in range(edge_index_start, edge_index_end):
-            result = pool.apply_async(build_instances,
-                                      (
-                                          source_path, target_path, 
-                                          source_edges[edge_index], target_edges[edge_index],
-                                          source_vocabulary, target_vocabulary,
-                                          structure,
-                                      ))
+            corpora_edge = dict({side: corpora_edges[side][edge_index] for side in sides})
+            result = pool.apply_async(build_instances, (primary_side, sides, paths, vocabularies, corpora_edge))
             results.append(result)
         for result in results:
             add_instances(result.get())
 
     return partial_dataset
 
-def build_dataset(args, dataset_name,
-                  source_path, target_path,
-                  source_vocabulary, target_vocabulary,
-                  number_worker, number_slice,
-                  logger):
-    source_edges = file_slice_edges(source_path, number_worker * number_slice)
-    target_edges = get_coedges(source_path, source_edges, target_path)
-    logger.info(f' * {dataset_name} will be slicing to {number_slice} partial_datasets!')
+
+def build_dataset(dataset_type, primary_side, sides, paths, vocabularies, logger,
+                  number_worker=1, number_slice=1):
+    primary_path = getattr(paths, primary_side)
+    primary_edges = file_slice_edges(primary_path, number_worker * number_slice)
+
+    corpora_edges = dict()
+    corpora_edges[primary_side] = primary_edges
+    for side in sides:
+        if side == primary_side:
+            continue
+        path = getattr(paths, side)
+        edges = get_coedges(primary_path, primary_edges, path)
+        corpora_edges[side] = edges
+
+    logger.info(f' * {dataset_type} will be slicing to {number_slice} partial_datasets!')
     datasets = list()
     for index in range(number_slice):
         edge_index_start = index * number_worker
         edge_index_end = edge_index_start + number_worker
 
-        logger.info(f' * Building No.{index} partial_dataset ...')
-        dataset = get_partial_dataset(source_path, target_path,
-                                      source_vocabulary, target_vocabulary,
-                                      source_edges, target_edges,
-                                      edge_index_start, edge_index_end,
-                                      number_worker)
+        logger.info(f' ** Building No.{index} partial_dataset ...')
+        dataset = get_partial_dataset(primary_side, sides, paths, vocabularies, corpora_edges,
+                                      edge_index_start, edge_index_end, number_worker)
 
-        logger.info(f' * {len(dataset)} instances  Built.')
+        logger.info(f'    {len(dataset)} instances Built.')
         datasets.append(dataset)
     logger.info(f' * Total {sum(len(dataset) for dataset in datasets)} instances.')
+
     return datasets
 
 
 def preprocess(args):
     logger = logging.get_logger(logging_path=args.logging_path)
 
-    data_directory = args.data_directory
-    if not os.path.isdir(data_directory):
+    fix_random_procedure(args.random_seed)
+
+    if not os.path.isdir(args.data.directory):
         logger.error('Data directory does not exists!')
         return
 
-    source_language = args.language.source
-    target_language = args.language.target
-    logger.info(f'source language: {source_language}')
-    logger.info(f'target language: {target_language}')
-
     logger.info('Building vocabulary ...')
-    source_vocabulary, target_vocabulary = build_vocabulary(args, args.corpora.train_path.source, args.corpora.train_path.target, args.number_worker, logger)
+    vocabularies = build_vocabularies(
+        args.data.sides,
+        args.vocabulary.groups,
+        args.vocabulary.sizes,
+        args.corpus.train_paths,
+        args.data.languages,
+        args.data.number_worker,
+        logger
+    )
 
-    logger.info(' > Saving vocabulary ...')
-    source_vocab_path = os.path.join(data_directory, f'{source_language}-{target_language}.{source_language}.vocab')
-    target_vocab_path = os.path.join(data_directory, f'{source_language}-{target_language}.{target_language}.vocab')
-    save_data_objects(source_vocab_path, [source_vocabulary, ])
-    logger.info(f' > Saved {source_vocab_path} !')
-    save_data_objects(target_vocab_path, [target_vocabulary, ])
-    logger.info(f' > Saved {target_vocab_path} !')
+    logger.info(' > Saving vocabularies ...')
+    vocabs_path = os.path.join(args.data.directory, f'{args.data.name}.vocab')
+    save_data_objects(vocabs_path, [vocabularies, ])
+    logger.info(f' > Saved {vocabs_path} !')
 
     logger.info('Building training dataset ...')
-    training_dataset = build_dataset(args, 'train',
-                                     args.corpora.train_path.source, args.corpora.train_path.target,
-                                     source_vocabulary, target_vocabulary,
-                                     args.number_worker, args.number_slice,
-                                     logger)
+    training_dataset = build_dataset(
+        'train',
+        args.data.primary_side,
+        args.data.sides,
+        args.corpus.train_paths,
+        vocabularies,
+        logger,
+        args.data.number_worker,
+        args.data.number_slice,
+    )
 
     logger.info(' > Saving training dataset ...')
-    training_dataset_name = f'train.{source_language}-{target_language}.dataset'
-    training_dataset_path = os.path.join(data_directory, training_dataset_name)
+    training_dataset_path = os.path.join(args.data.directory, f'{args.data.name}.train.dataset')
     save_data_objects(training_dataset_path, training_dataset)
     logger.info(f' > Saved {training_dataset_path} !')
 
     logger.info('Building validation dataset ...')
-    validation_dataset = build_dataset(args, 'valid',
-                                       args.corpora.valid_path.source, args.corpora.valid_path.target,
-                                       source_vocabulary, target_vocabulary,
-                                       1, 1,
-                                       logger)
+    validation_dataset = build_dataset(
+        'valid',
+        args.data.primary_side,
+        args.data.sides,
+        args.corpus.valid_paths,
+        vocabularies,
+        logger,
+    )
 
     logger.info(' > Saving validation dataset ...')
-    validation_dataset_name = f'valid.{source_language}-{target_language}.dataset'
-    validation_dataset_path = os.path.join(data_directory, validation_dataset_name)
+    validation_dataset_path = os.path.join(args.data.directory, f'{args.data.name}.valid.dataset')
     save_data_objects(validation_dataset_path, validation_dataset)
     logger.info(f' > Saved {validation_dataset_path} !')
 
@@ -206,7 +219,6 @@ def preprocess(args):
 
 def main():
     args = harg.get_arguments()
-    fix_random_procedure(args.seed)
     preprocess_args = harg.get_partial_arguments(args, 'binaries.preprocess')
     preprocess(preprocess_args)
 
