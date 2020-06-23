b055e488c9cfcd4bb122fc2aaa56887dd25f611d
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-06-23 17:14:18 +0800

    Add checkpoint utility

---
+++ ynmt/utilities/checkpoint.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-06-23 17:10
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+def load_checkpoint():
+    pass
+
+
+def save_checkpoint():
+    pass
