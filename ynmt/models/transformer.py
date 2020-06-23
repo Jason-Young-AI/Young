1536c5d6ef4f90a16be18c26aa98a1d98229a285
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-06-23 17:16:00 +0800

    Update Models

--- ynmt/models/transformer.py
+++ ynmt/models/transformer.py
@@ -8,3 +8,17 @@
 #
 # This source code is licensed under the Apache-2.0 license found in the
 # LICENSE file in the root directory of this source tree.
+
+
+import torch
+
+
+from ynmt.modules import xxx
+
+
+def build_transformer(args, vocabularies):
+    encoder = 
+
+
+class Transformer(torch.nn.module):
+    pass
