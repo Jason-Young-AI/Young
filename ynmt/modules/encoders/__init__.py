bffd0c6e6f445596d7ab3311d3eb649a5febdfe9
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-05 18:48:59 +0800

    rename

--- ynmt/modules/encoders/__init__.py
+++ ynmt/modules/encoders/__init__.py
@@ -8,3 +8,6 @@
 #
 # This source code is licensed under the Apache-2.0 license found in the
 # LICENSE file in the root directory of this source tree.
+
+
+from ynmt.modules.encoders.transformer_encoder import TransformerEncoder
