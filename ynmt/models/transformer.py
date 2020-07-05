f22bb82df713d8defd12fcb77e9e1741703d2d95
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-05 18:50:31 +0800

    Finish Transformer

--- ynmt/models/transformer.py
+++ ynmt/models/transformer.py
@@ -17,16 +17,35 @@ from ynmt.modules.encoders import TransformerEncoder
 from ynmt.modules.decoders import TransformerDecoder
 
 
-def build_transformer(args, vocabularies, checkpoint):
-    encoder = 
+def build_model_transformer(args, vocabularies, checkpoint):
+    transformer_encoder = TransformerEncoder(
+        vocabularies['source'],
+        args.encoder.layer_number, args.dimension, args.feedforward_dimension, args.head_number,
+        args.dropout_probability, args.attention_dropout_probability, args.feedforward_dropout_probability
+    )
+    transformer_decoder = TransformerDecoder(
+        vocabularies['target'],
+        args.decoder.layer_number, args.dimension, args.feedforward_dimension, args.head_number,
+        args.dropout_probability, args.attention_dropout_probability, args.feedforward_dropout_probability
+    )
+
+    transformer = Transformer(transformer_encoder, transformer_decoder)
+
+    if checkpoint is not None:
+        transformer.load_state_dict(checkpoint['model'], strict=False)
+
+    return transformer
 
 
 class Transformer(torch.nn.module):
-    def __init__(self, ):
+    def __init__(self, encoder, decoder):
         super(Transformer, self).__init__()
-        self.encoder = TransformerEncoder
-        self.decoder = TransformerDecoder
+        self.encoder = encoder
+        self.decoder = decoder
 
+    def forward(self, source, target, source_length, target_length):
+        codes = self.encoder(source, source_length)
 
-    def forward(self, source, target):
-        pass
+        prediction, _, _ = self.decoder(codes, source_length, target[:-1], target_length)
+
+        return prediction
