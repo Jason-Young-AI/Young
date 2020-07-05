4c86d031c85874820e1b3bc07aaba0e85bf92a5b
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-05 18:46:40 +0800

    Add embeddings package

---
+++ ynmt/modules/embeddings/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-06-28 14:27
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from ynmt.modules.embeddings.learned_positional_embedding import LearnedPositionalEmbedding
+from ynmt.modules.embeddings.trigonometric_positional_embedding import TrigonometricPositionalEmbedding
