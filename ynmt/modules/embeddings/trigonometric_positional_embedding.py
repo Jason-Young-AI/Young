e4908c9b65384c0102ebbae1675b8e00e9a823aa
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-22 18:17:22 +0800

    Finish PosEmb

--- ynmt/modules/embeddings/trigonometric_positional_embedding.py
+++ ynmt/modules/embeddings/trigonometric_positional_embedding.py
@@ -15,18 +15,26 @@ import torch
 
 
 class TrigonometricPositionalEmbedding(torch.nn.Module):
-    def __init__(self, embedding_number, dimension):
-        position = torch.arange(0, embedding_number).unsqueeze(1)
-        sin_multiplicator = torch.exp(-(math.log(10000) / dimension) * 2 * torch.arange(0, dimension, 2))
-        cos_multiplicator = torch.exp(-(math.log(10000) / dimension) * 2 * torch.arange(1, dimension, 2))
+    def __init__(self, embedding_number, dimension, padding_idx=None):
+        super(TrigonometricPositionalEmbedding, self).__init__()
+        self.embedding_number = embedding_number
+        self.dimension = dimension
+        self.padding_idx = padding_idx
+        position = torch.arange(0, self.embedding_number).unsqueeze(1)
+        sin_multiplicator = torch.exp(-(math.log(10000) / self.dimension) * 2 * torch.arange(0, self.dimension, 2))
+        cos_multiplicator = torch.exp(-(math.log(10000) / self.dimension) * 2 * torch.arange(1, self.dimension, 2))
         sin_weight = torch.sin(position * sin_multiplicator)
         cos_weight = torch.cos(position * cos_multiplicator)
 
-        weight = torch.zeros(embedding_number, dimension)
+        weight = torch.zeros(self.embedding_number, self.dimension)
         weight[:, 0::2] = sin_weight
         weight[:, 1::2] = cos_weight
         weight[self.padding_idx] = 0
         self.register_buffer('weight', weight)
 
     def forward(self, position):
-        torch.index_select(self.weight, 0, position)
+        size = list(position.size())
+        size.append(self.dimension)
+        embedded_position = torch.index_select(self.weight, 0, position.reshape(-1))
+        embedded_position = embedded_position.view(size)
+        return embedded_position
