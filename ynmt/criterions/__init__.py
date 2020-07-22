f29241ddd4cf2d755f6cd52b01c7394206be558f
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-22 18:08:29 +0800

    Move Criterion Base Class to a independent file.

--- ynmt/criterions/__init__.py
+++ ynmt/criterions/__init__.py
@@ -10,32 +10,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import torch
+from ynmt.criterions.criterion import Criterion
+from ynmt.criterions.label_smoothing_cross_entropy import build_criterion_label_smoothing_cross_entropy
+from ynmt.criterions.cross_entropy import build_criterion_cross_entropy
 
 
-class Criterion(torch.nn.Module):
-    def __init__(self, label_number, ignore_index):
-        super(Criterion, self).__init__()
-        self.label_number = label_number
-        self.ignore_index = ignore_index
-
-    def forward(self, predicted_distribution, ground_truth, reduction='none'):
-        assert predicted_distribution.dim()-1 == ground_truth.dim()
-        assert predicted_distribution.size()[:-1] == ground_truth.size()
-        assert predicted_distribution.size(-1) == self.label_number
-
-        ground_truth = ground_truth.unsqueeze(-1)
-
-        mask = torch.eq(ground_truth, self.ignore_index)
-
-        loss, states = self.compute_loss(predicted_distribution, ground_truth, mask)
-
-        if reduction == 'sum':
-            loss = torch.sum(loss)
-        if reduction == 'mean':
-            loss = torch.mean(loss)
-
-        return loss, states
-
-    def compute_loss(predicted_distribution, ground_truth, mask):
-        raise NotImplementedError
+def build_criterion(args, vocabulary):
+    criterion = globals()[f'build_criterion_{args.name}'](args, vocabulary)
+    return criterion
---
+++ ynmt/criterions/criterion.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-06-30 09:38
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import torch
+
+
+class Criterion(torch.nn.Module):
+    def __init__(self, label_number, ignore_index):
+        super(Criterion, self).__init__()
+        self.label_number = label_number
+        self.ignore_index = ignore_index
+
+    def forward(self, predicted_distribution, ground_truth, reduction='none'):
+        assert predicted_distribution.dim() - 1 == ground_truth.dim(), f'Wrong number of dimension: #1 arg:{predicted_distribution.size()}, #2 arg: {ground_truth.size()}'
+        assert predicted_distribution.size()[:-1] == ground_truth.size(), f'Wrong size: #1 arg:{predicted_distribution.size()}, #2 arg: {ground_truth.size()}'
+        assert predicted_distribution.size(-1) == self.label_number, f'Number of label should be {self.label_number} instead: #1 arg:{predicted_distribution.size(-1)}'
+
+        ground_truth = ground_truth.unsqueeze(-1)
+
+        mask = torch.eq(ground_truth, self.ignore_index)
+
+        loss, states = self.compute_loss(predicted_distribution, ground_truth, mask)
+
+        if reduction == 'sum':
+            loss = torch.sum(loss)
+        if reduction == 'mean':
+            loss = torch.mean(loss)
+
+        return loss, states
+
+    def compute_loss(predicted_distribution, ground_truth, mask):
+        raise NotImplementedError
