bb7b058352f12ee4db5e897a9004581fd8a2e5fd
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-22 18:07:16 +0800

    Fix bug of MHAttn's Mask

--- ynmt/modules/attentions/multihead_attention.py
+++ ynmt/modules/attentions/multihead_attention.py
@@ -15,7 +15,7 @@ import torch
 
 
 class MultiHeadAttention(torch.nn.Module):
-    def __init__(self, dimension, head_number, dropout_probability)
+    def __init__(self, dimension, head_number, dropout_probability):
         super(MultiHeadAttention, self).__init__()
         head_dimension, remaind_dimension = divmod(dimension,  head_number)
         assert remaind_dimension== 0, "An Error occured during initialization of MultiHeadAttention."
@@ -28,30 +28,31 @@ class MultiHeadAttention(torch.nn.Module):
         self.value_linear = torch.nn.Linear(self.dimension, self.head_number * self.head_dimension)
 
         self.softmax = torch.nn.Softmax(dim=-1)
+
         self.dropout = torch.nn.Dropout(dropout_probability)
 
         self.attention_linear = torch.nn.Linear(self.dimension, self.head_number * self.head_dimension)
 
     def forward(self, query, key, value, attention_weight_mask):
-        query_length, batch_size, dimension = query
+        query_length, batch_size, dimension = query.size()
 
         def split(x):
-            return x.reshape(-1, batch_size, self.head_number, self.head_dimension).transpose(0, 1)
+            return x.reshape(-1, batch_size, self.head_number, self.head_dimension).transpose(0, 1).transpose(1, 2)
 
         def merge(x):
-            return x.transpose(0, 1).reshape(-1, batch_size, self.head_number * self.head_dimension)
+            return x.transpose(1, 2).transpose(0, 1).reshape(-1, batch_size, self.head_number * self.head_dimension)
 
         query = self.query_linear(query)
-        key = self.value_linear(key)
+        key = self.key_linear(key)
         value = self.value_linear(value)
 
         query = split(query)
         key = split(key)
         value = split(value)
 
-        attention_weight = torch.matmul(query, key.transpose(1, 2)) / math.sqrt(self.head_dimension)
-        attention_weight = self.dropout(self.softmax(attention_weight)))
+        attention_weight = torch.matmul(query, key.transpose(2, 3)) / math.sqrt(self.head_dimension)
         attention_weight = attention_weight.masked_fill(attention_weight_mask.unsqueeze(1), float("-inf"))
+        attention_weight = self.dropout(self.softmax(attention_weight))
         attention = torch.matmul(attention_weight, value)
 
         attention = merge(attention)
