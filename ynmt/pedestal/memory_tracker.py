cf31e4e006a9904df8f7598af5d93b41fa6c97bf
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-24 17:11:55 +0800

    Add MemoryTracker

---
+++ ynmt/pedestal/memory_tracker.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-07-24 16:37
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import gc
+import pynvml
+import inspect
+import datetime
+
+import torch
+import numpy as np
+
+
+class MemoryTracker(object):
+    def __init__(self, frame, detail=True, verbose=False, device=0, path=''):
+        assert inspect.isframe(frame), f'#1 Argument should be a Frame object.'
+
+        self.frame = frame
+        self.detail = detail
+        self.verbose = verbose
+        self.device = device
+
+        self.last_tensor_sizes = set()
+        self.gpu_profile_path = path + f'{datetime.datetime.now():%d-%b-%y-%H:%M:%S}-gpu_mem_track.txt'
+        self.begin = True
+
+        self.func_name = frame.f_code.co_name
+        self.filename = frame.f_globals["__file__"]
+        if (self.filename.endswith(".pyc") or
+                self.filename.endswith(".pyo")):
+            self.filename = self.filename[:-1]
+        self.module_name = self.frame.f_globals["__name__"]
+        self.curr_line = self.frame.f_lineno
+
+    def get_tensors(self):
+        for obj in gc.get_objects():
+            try:
+                if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                    tensor = obj
+                else:
+                    continue
+                if tensor.is_cuda:
+                    yield tensor
+            except Exception as e:
+                if self.verbose:
+                    print('A trivial exception occured: {}'.format(e))
+
+    def track(self):
+        """
+        Track the GPU memory usage
+        """
+        pynvml.nvmlInit()
+        handle = pynvml.nvmlDeviceGetHandleByIndex(self.device)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        self.curr_line = self.frame.f_lineno
+        where_str = self.module_name + ' ' + self.func_name + ':' + ' line ' + str(self.curr_line)
+
+        with open(self.gpu_profile_fn, 'a+') as f:
+
+            if self.begin:
+                f.write(f"GPU Memory Track | {datetime.datetime.now():%d-%b-%y-%H:%M:%S} |"
+                        f" Total Used Memory:{meminfo.used/1000**2:<7.1f}Mb\n\n")
+                self.begin = False
+
+            if self.print_detail is True:
+                ts_list = [tensor.size() for tensor in self.get_tensors()]
+                new_tensor_sizes = {(type(x), tuple(x.size()), ts_list.count(x.size()), np.prod(np.array(x.size()))*4/1000**2)
+                                    for x in self.get_tensors()}
+                for t, s, n, m in new_tensor_sizes - self.last_tensor_sizes:
+                    f.write(f'+ | {str(n)} * Size:{str(s):<20} | Memory: {str(m*n)[:6]} M | {str(t):<20}\n')
+                for t, s, n, m in self.last_tensor_sizes - new_tensor_sizes:
+                    f.write(f'- | {str(n)} * Size:{str(s):<20} | Memory: {str(m*n)[:6]} M | {str(t):<20} \n')
+                self.last_tensor_sizes = new_tensor_sizes
+
+            f.write(f"\nAt {where_str:<50}"
+                    f"Total Used Memory:{meminfo.used/1000**2:<7.1f}Mb\n\n")
+
+        pynvml.nvmlShutdown()
+
