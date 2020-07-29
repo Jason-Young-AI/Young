8f4e5e2271f0642a2e931ef942d3fc3715768391
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-07-29 11:56:31 +0800

    Update MLP

--- ynmt/modules/perceptrons/multilayer_perceptron.py
+++ ynmt/modules/perceptrons/multilayer_perceptron.py
@@ -15,16 +15,12 @@ import torch
 
 non_linear_dim = set({'Softmax', 'LogSoftmax', 'Softmin'})
 non_linear_nodim = set({'ReLU', 'Tanh', 'Sigmoid'})
-non_linear = set()
-non_linear.update(non_linear_dim)
-non_linear.update(non_linear_nodim)
 
 
 class MultilayerPerceptron(torch.nn.Module):
-    def __init__(self, input_dimension, output_dimension, dimensions=list(), activation='Softmax'):
+    def __init__(self, input_dimension, output_dimension, dimensions=list(), activation=None):
         super(MultilayerPerceptron, self).__init__()
         assert isinstance(dimensions, list), f'Dimensions should be List()'
-        assert activation in non_linear, 'Activation {activation} not supported yet'
 
         input_dimensions = [input_dimension] + dimensions
         output_dimensions = dimensions + [output_dimension]
@@ -34,10 +30,16 @@ class MultilayerPerceptron(torch.nn.Module):
         for input_dimension, output_dimension in self.io_sizes:
             linear_layer = torch.nn.Linear(input_dimension, output_dimension)
             self.linear_layers.append(linear_layer)
+
         if activation in non_linear_dim:
             self.activation = getattr(torch.nn, activation)(dim=-1)
+        elif activation in non_linear_nodim:
+            if activation == 'ReLU':
+                self.activation = getattr(torch.nn, activation)(inplace=True)
+            else:
+                self.activation = getattr(torch.nn, activation)()
         else:
-            self.activation = getattr(torch.nn, activation)()
+            self.activation = lambda x: x
             
     def forward(self, x):
         for linear_layer in self.linear_layers:
