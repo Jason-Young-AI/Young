efa4091b64d9788f17c0821df08c92725787e4a4
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-08-12 12:08:04 +0800

    Add BaseModel

---
+++ ynmt/models/model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2020-08-11 17:12
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import torch
+
+
+class Model(torch.nn.Module):
+    def __init__(self, settings):
+        super(Model, self).__init__()
+        self.settings = settings
