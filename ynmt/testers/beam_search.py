e08d052837f1ea04f5919aba31bd9f2552905b0d
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-08-13 23:58:03 +0800

    Fix bugs

--- ynmt/testers/beam_search.py
+++ ynmt/testers/beam_search.py
@@ -46,153 +46,166 @@ class BeamSearch(object):
         self.beta = beta
 
         self.parallel_line_number = None
-        self.adjacent_node_getter = None
 
-    def initialize(self, parallel_line_number, adjacent_node_states_getter):
+    def initialize(self, parallel_line_number, device_descriptor):
         self.current_depth = 0
         self.parallel_line_number = parallel_line_number
-        self.adjacent_node_states_getter = adjacent_node_states_getter
 
-        self.reserved_pathes = torch.full(
+        self.reserved_paths = torch.full(
             [self.parallel_line_number, self.reserved_path_number, 1],
             self.initial_node,
-            dtype=torch.long
+            dtype=torch.long,
+            device=device_descriptor
         )
 
         self.reserved_path_log_probs = torch.full(
             [self.parallel_line_number, self.reserved_path_number],
             float("-inf"),
-            dtype=torch.float
+            dtype=torch.float,
+            device=device_descriptor
         )
         self.reserved_path_log_probs[:, 0] = 0
 
         self.reserved_path_scores = torch.full(
             [self.parallel_line_number, self.reserved_path_number],
             float("-inf"),
-            dtype=torch.float
+            dtype=torch.float,
+            device=device_descriptor
         )
         self.reserved_path_scores[:, 0] = 0
 
-        self.candidate_pathes = list(
+        self.candidate_paths = list(
             list()
             for _ in range(self.parallel_line_number)
         )
 
-        self.active_line_indices = torch.arange(self.parallel_line_number)
-        self.line_order = torch.arange(self.parallel_line_number)
-        self.finished_lines = torch.full([self.parallel_line_number], False, dtype=torch.bool)
+        self.active_line_indices = torch.arange(self.parallel_line_number, device=device_descriptor)
+        self.line_order = torch.arange(self.parallel_line_number, device=device_descriptor)
+        self.finished_lines = torch.full(
+            [self.parallel_line_number],
+            False,
+            dtype=torch.bool,
+            device=device_descriptor
+        )
+        self.path_offset = torch.arange(
+            0,
+            self.parallel_line_number * self.reserved_path_number,
+            self.reserved_path_number,
+            device=device_descriptor
+        ).unsqueeze(-1).repeat(1, self.reserved_path_number)
+
+    @property
+    def current_nodes(self):
+        return self.reserved_paths[:, :, -1]
+
+    @property
+    def found_nodes(self):
+        return self.reserved_paths
 
     @property
     def finished(self):
-        finished_pathes = self.reserved_pathes[:, :, -1].eq(self.terminal_node)
-        if self.current_depth == self.max_depth:
-            finished_pathes.fill_(True)
+        finished_paths = self.current_nodes.eq(self.terminal_node)
 
-        self.finished_lines = self.finished_lines | finished_pathes[:, 0].eq(True)
+        if self.current_depth == self.max_depth + 1:
+            finished_paths.fill_(True)
+
+        self.finished_lines |= finished_paths[:, 0].eq(True)
+
         new_active_line_indices = []
-        for line_index, finished_path in enumerate(finished_pathes):
+        for line_index in range(finished_paths.size(0)):
             active_line_index = self.active_line_indices[line_index]
-            for path_index in finished_path.nonzero():
-                print(finished_pathes.size())
-                print(line_index)
-                print(active_line_index)
-                self.candidate_pathes[active_line_index].append(
+            finished_path = finished_paths[line_index].nonzero().view(-1)
+            for path_index in finished_path:
+                self.candidate_paths[active_line_index].append(
                     dict(
-                        prob = self.reserved_path_log_probs[line_index][path_index],
-                        score = self.reserved_path_scores[line_index][path_index],
-                        path = self.reserved_pathes[line_index][path_index]
+                        log_prob = self.reserved_path_log_probs[line_index, path_index].item(),
+                        score = self.reserved_path_scores[line_index, path_index].item(),
+                        path = self.reserved_paths[line_index, path_index, 1:]
                     )
                 )
-            if len(self.candidate_pathes[active_line_index]) >= self.candidate_path_number and self.finished_lines[line_index]:
-                self.candidate_pathes[active_line_index] = sorted(self.candidate_pathes[active_line_index], key=lambda x: x['score'], reverse=True)
-                self.candidate_pathes[active_line_index] = self.candidate_pathes[active_line_index][:self.candidate_path_number]
+            if self.finished_lines[line_index] and len(self.candidate_paths[active_line_index]) >= self.candidate_path_number:
+                self.candidate_paths[active_line_index] = sorted(self.candidate_paths[active_line_index], key=lambda x: x['score'], reverse=True)
+                self.candidate_paths[active_line_index] = self.candidate_paths[active_line_index][:self.candidate_path_number]
                 self.parallel_line_number -= 1
             else:
                 new_active_line_indices.append(line_index)
 
-        new_active_line_indices = torch.tensor(new_active_line_indices, dtype=torch.long)
+        new_active_line_indices = torch.tensor(
+            new_active_line_indices,
+            dtype=torch.long,
+            device=self.active_line_indices.device
+        )
 
         self.active_line_indices = torch.index_select(self.active_line_indices, 0, new_active_line_indices)
         self.reserved_path_log_probs = torch.index_select(self.reserved_path_log_probs, 0, new_active_line_indices)
         self.reserved_path_scores = torch.index_select(self.reserved_path_scores, 0, new_active_line_indices)
-        self.reserved_pathes = torch.index_select(self.reserved_pathes, 0, new_active_line_indices)
+        self.reserved_paths = torch.index_select(self.reserved_paths, 0, new_active_line_indices)
         self.finished_lines = torch.index_select(self.finished_lines, 0, new_active_line_indices)
+        self.path_offset = torch.index_select(self.path_offset, 0, new_active_line_indices)
 
         if len(new_active_line_indices) == 0:
             return True
         else:
             return False
 
-    def get_adjacent_node_states(self, nodes):
-        adjacent_node_states = self.adjacent_node_states_getter(nodes)
+    def search(self, adjacent_node_log_probs):
+        assert adjacent_node_log_probs.size(0) == self.parallel_line_number
+        assert adjacent_node_log_probs.size(1) == self.reserved_path_number
+        assert adjacent_node_log_probs.size(2) == self.search_space_size
+        if self.current_depth <= self.min_depth:
+            adjacent_node_log_probs[:, :, self.terminal_node] = float("-inf")
 
-        adjacent_node_probs = adjacent_node_states['prob']
-        assert adjacent_node_probs.size(0) == self.parallel_line_number
-        assert adjacent_node_probs.size(1) == self.reserved_path_number
-        assert adjacent_node_probs.size(2) == self.search_space_size
+        adjacent_node_log_probs = adjacent_node_log_probs.reshape(-1, self.search_space_size)
 
-        if self.current_depth < self.min_depth:
-            adjacent_node_probs[:, :, self.terminal_node] = 0
+        extended_path_log_probs = self.reserved_path_log_probs.reshape(-1, 1) + adjacent_node_log_probs
+        extended_path_log_probs = extended_path_log_probs.reshape(self.parallel_line_number, -1)
 
-        adjacent_node_states = dict(
-            prob = adjacent_node_probs
+        length_normalization = ((5 + self.current_depth)/(5 + 1)) ** self.alpha
+        coverage_penalty = self.beta
+
+        extended_path_scores = extended_path_log_probs / length_normalization + coverage_penalty
+
+        topk_extended_path_scores, topk_extended_path_indices = torch.topk(
+            extended_path_scores,
+            self.reserved_path_number,
         )
-        return adjacent_node_states
 
-    def search(self):
-        while not self.finished:
-            nodes = self.reserved_pathes[:, :, -1]
-            adjacent_node_states = self.get_adjacent_node_states(nodes)
-            adjacent_node_probs = adjacent_node_states['prob']
-            adjacent_node_log_probs = torch.log(adjacent_node_probs).reshape(-1, self.search_space_size)
+        assert self.reserved_path_scores.size() == topk_extended_path_scores.size()
+        self.reserved_path_scores = topk_extended_path_scores
 
-            extended_path_log_probs = self.reserved_path_log_probs.reshape(-1, 1) + adjacent_node_log_probs
-            extended_path_log_probs = extended_path_log_probs.reshape(self.parallel_line_number, -1)
+        # update reserved_path_log_probs
+        line_offset = topk_extended_path_indices + \
+                      self.line_order[:self.parallel_line_number].unsqueeze(1) * \
+                      self.reserved_path_number * \
+                      self.search_space_size
 
-            length_normalization = ((5 + self.current_depth)/(5 + 1)) ** self.alpha
-            coverage_penalty = self.beta
+        self.reserved_path_log_probs = torch.index_select(
+            extended_path_log_probs.reshape(-1),
+            0,
+            line_offset.reshape(-1)
+        ).reshape(self.parallel_line_number, self.reserved_path_number)
 
-            extended_path_scores = extended_path_log_probs / length_normalization + coverage_penalty
+        # update reserved_paths
+        adjacent_nodes = torch.fmod(topk_extended_path_indices, self.search_space_size)
 
-            topk_extended_path_scores, topk_extended_path_indices = torch.topk(
-                extended_path_scores,
-                self.reserved_path_number,
-            )
+        self.path_offset = torch.floor_divide(topk_extended_path_indices, self.search_space_size) + \
+                      self.line_order[:self.parallel_line_number].unsqueeze(1) * \
+                      self.reserved_path_number
 
-            self.reserved_path_scores = topk_extended_path_scores
+        self.reserved_paths = torch.index_select(
+            self.reserved_paths.reshape(self.parallel_line_number * self.reserved_path_number, -1),
+            0,
+            self.path_offset.reshape(-1)
+        ).reshape(self.parallel_line_number, self.reserved_path_number, -1)
 
-            # update reserved_path_log_probs
-            line_offset = topk_extended_path_indices + \
-                          self.line_order[:self.parallel_line_number].unsqueeze(1) * \
-                          self.reserved_path_number * \
-                          self.search_space_size
+        self.reserved_paths = torch.cat(
+            [
+                self.reserved_paths,
+                adjacent_nodes.unsqueeze(-1)
+            ],
+            -1
+        )
 
-            self.reserved_path_log_probs = torch.index_select(
-                extended_path_log_probs.reshape(-1),
-                0,
-                line_offset.reshape(-1)
-            ).reshape(self.parallel_line_number, self.reserved_path_number)
-
-            # update reserved_pathes
-            adjacent_nodes = torch.fmod(topk_extended_path_indices, self.search_space_size)
-
-            path_offset = torch.floor_divide(topk_extended_path_indices, self.search_space_size) + \
-                          self.line_order[:self.parallel_line_number].unsqueeze(1) * \
-                          self.reserved_path_number
-
-            self.reserved_pathes = torch.index_select(
-                self.reserved_pathes.reshape(self.parallel_line_number * self.reserved_path_number, -1),
-                0,
-                path_offset.reshape(-1)
-            ).reshape(self.parallel_line_number, self.reserved_path_number, -1)
-            self.reserved_pathes = torch.cat(
-                [
-                    self.reserved_pathes,
-                    adjacent_nodes.unsqueeze(-1)
-                ],
-                -1
-            )
-
-            self.current_depth += 1
+        self.current_depth += 1
 
         return
