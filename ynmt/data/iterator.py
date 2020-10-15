d1c17414d740498c72f74abd89cc2c30e1a20f5c
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-10-16 00:05:53 +0800

    Optimize the iteration of datasets.

--- ynmt/data/iterator.py
+++ ynmt/data/iterator.py
@@ -13,82 +13,86 @@
 import random
 
 from ynmt.data.batch import Batch
-from ynmt.data.instance import InstanceComparator
 from ynmt.utilities.file import load_datas
 from ynmt.utilities.random import shuffled
+from ynmt.utilities.statistics import Statistics
 
 
 class Iterator(object):
     def __init__(self,
-        dataset_path, batch_size,
-        instance_size_calculator, instance_filter=None, instance_comparator=InstanceComparator(),
-        accumulate_number=1, mode='preserve', infinite=False,
+        dataset_path, container_size, dock_size=1, export_volume=1,
+        instance_filter=None, instance_comparator=None, instance_size_calculator=None,
+        shuffle=False, infinite=False,
     ):
-        assert mode in {'preserve', 'ascend', 'descend', 'shuffle'}, "Wrong choice of order."
-
         self.dataset_path = dataset_path
-        self.batch_size = batch_size
-        self.instance_size_calculator = instance_size_calculator
+        self.container_size = container_size
+        self.dock_size = dock_size
+        self.export_volume = export_volume
+
         self.instance_filter = instance_filter
         self.instance_comparator = instance_comparator
-        self.accumulate_number = accumulate_number
-        self.mode = mode
+        self.instance_size_calculator = instance_size_calculator
+
         self.infinite = infinite
+
+        self.shuffle = shuffle
         self.random_state = random.getstate()
+
+    def pack_instances(self, instances, size):
+        packed_instances = list()
+        for instance in instances:
+            packed_instances.append(instance)
+            packed_instances_size = self.instance_size_calculator(packed_instances)
+
+            if packed_instances_size > size:
+                yield packed_instances[:-1]
+                packed_instances = packed_instances[-1:]
+                packed_instances_size = self.instance_size_calculator(packed_instances)
+
+            if packed_instances_size == size:
+                yield packed_instances
+                packed_instances = list()
+                packed_instances_size = 0
+
+        if len(packed_instances) != 0:
+            yield packed_instances
 
     @property
     def instances(self):
         for dataset in load_datas(self.dataset_path):
-            if self.mode == 'preserve':
-                instances = dataset
-            elif self.mode == 'ascend':
-                instances = sorted(dataset, key=self.instance_comparator, reverse=False)
-            elif self.mode == 'descend':
-                instances = sorted(dataset, key=self.instance_comparator, reverse=True)
-            elif self.mode == 'shuffle':
-                instances = shuffled(dataset)
-
-            for instance in instances:
-                yield instance
+            if self.shuffle:
+                dataset = shuffled(dataset)
+            for index, instance in enumerate(dataset):
+                if self.instance_filter is None or not self.instance_filter(instance):
+                    yield instance
 
     @property
-    def batches(self):
-        accum_batches = list()
-        current_instances = list()
-        max_size = 0
-        for instance in self.instances:
-            if self.instance_filter is not None and self.instance_filter(instance):
-                continue
-            current_instances.append(instance)
-            max_size = max(max_size, self.instance_size_calculator(instance).max())
-            total_size = len(current_instances) * max_size
-            if total_size < self.batch_size:
-                continue
-            else:
-                if total_size > self.batch_size:
-                    accum_batches.append(Batch(instance.structure, current_instances[:-1]))
-                    current_instances = current_instances[-1:]
-                    max_size = self.instance_size_calculator(current_instances[-1]).max()
-                else:
-                    accum_batches.append(Batch(instance.structure, current_instances))
-                    current_instances = list()
-                    max_size = 0
-            if len(accum_batches) == self.accumulate_number:
-                yield accum_batches
-                accum_batches = list()
+    def containers(self):
+        docks = self.pack_instances(self.instances, self.container_size * self.dock_size)
+        for dock in docks:
+            if self.instance_comparator is not None and self.shuffle:
+                dock = sorted(dock, key=self.instance_comparator)
 
-        if len(current_instances) != 0:
-            yield [Batch(current_instances[-1].structure, current_instances)]
+            containers = self.pack_instances(dock, self.container_size)
+            if self.shuffle:
+                containers = shuffled(list(containers))
+
+            for container in containers:
+                assert len(container) != 0, f'Container size is too small, packed no instance, please size up!{container}'
+                yield Batch(container[-1].structure, container)
 
     def __iter__(self):
         original_random_state = random.getstate()
         random.setstate(self.random_state)
+
+        exports = list()
         while True:
-            for batch in self.batches:
-                if len(batch) == 0:
-                    raise ValueError('Batch size too small, so batching no instance, please size up!')
-                else:
-                    yield batch
+            for container in self.containers:
+                exports.append(container)
+                if len(exports) == self.export_volume:
+                    yield exports
+                    exports = list()
+
             if not self.infinite:
                 break
 
@@ -96,12 +100,12 @@ class Iterator(object):
 
 
 class RawTextIterator(object):
-    def __init__(self, raw_text_paths, instance_handler, batch_size, instance_size_calculator):
+    def __init__(self, raw_text_paths, instance_handler, batch_size, batch_size_calculator):
         assert isinstance(raw_text_paths, list), f'raw_text_paths must be a list of path.'
         self.raw_text_paths = raw_text_paths
         self.instance_handler = instance_handler
         self.batch_size = batch_size
-        self.instance_size_calculator = instance_size_calculator
+        self.batch_size_calculator = batch_size_calculator
 
     @property
     def instances(self):
@@ -122,19 +126,18 @@ class RawTextIterator(object):
 
         for instance in self.instances:
             current_instances.append(instance)
-            max_size = max(max_size, self.instance_size_calculator(instance).max())
-            total_size = len(current_instances) * max_size
-            if total_size < self.batch_size:
-                continue
-            else:
-                if total_size > self.batch_size:
-                    yield Batch(instance.structure, current_instances[:-1])
-                    current_instances = current_instances[-1:]
-                    max_size = self.instance_size_calculator(current_instances[-1]).max()
-                else:
-                    yield Batch(instance.structure, current_instances)
-                    current_instances = list()
-                    max_size = 0
+            current_batch_size = self.batch_size_calculator(current_instances)
+
+            if current_batch_size > self.batch_size:
+                yield Batch(instance.structure, current_instances[:-1])
+                current_instances = current_instances[-1:]
+                current_batch_size = self.batch_size_calculator(current_instances)
+
+            if current_batch_size == self.batch_size:
+                yield Batch(instance.structure, current_instances)
+                current_instances = list()
+                current_batch_size = 0
+
         if len(current_instances) != 0:
             yield Batch(current_instances[-1].structure, current_instances)
 
