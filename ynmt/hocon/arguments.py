b141573029655581e8a4970782e5b03002db723d
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-11-02 07:45:28 +0000

    Update HOCON settings

--- ynmt/hocon/arguments.py
+++ ynmt/hocon/arguments.py
@@ -12,101 +12,17 @@
 
 import os
 import sys
-import pyhocon
 import argparse
 import collections
 
-from ynmt.utilities.constant import Constant
-
-
-class HOCONArguments(object):
-    RESERVED_NAME = set({ '_HOCONArguments__names', 'dictionary', 'hocon', 'update', 'save' })
-    def __init__(self, hocon):
-        if isinstance(hocon, pyhocon.config_tree.ConfigTree):
-            self.__names = []
-            for name in hocon:
-                if name in HOCONArguments.RESERVED_NAME:
-                    raise ValueError(f'Invalid hocon attribute name(\'{name}\')')
-                self.__names.append(name)
-                if isinstance(hocon[name], pyhocon.config_tree.ConfigTree):
-                    setattr(self, name, HOCONArguments(hocon[name]))
-                else:
-                    setattr(self, name, hocon[name])
-        else:
-            raise ValueError(f'Argument hocon(\'{hocon}\') is not a \'pyhocon.config_tree.ConfigTree\' object.')
-
-    def __iter__(self):
-        for attribute_name in self.__names:
-            yield (attribute_name, self[attribute_name])
-
-    def __setitem__(self, attribute_name, attribute_value):
-        if attribute_name not in self.__names:
-            self.__names.add(attribute_name)
-        self.__dict__[attribute_name] = attribute_value
-
-    def __getitem__(self, attribute_name):
-        return self.__dict__[attribute_name]
-
-    @property
-    def dictionary(self):
-        dictionary = {}
-        for name in self.__names:
-            if isinstance(getattr(self, name), HOCONArguments):
-                dictionary[name] = getattr(self, name).dictionary
-            else:
-                dictionary[name] = getattr(self, name)
-        dictionary = collections.OrderedDict(sorted(dictionary.items(), key=lambda x: x[0]))
-        return dictionary
-
-    @property
-    def hocon(self):
-        hocon = pyhocon.ConfigFactory().from_dict(self.dictionary)
-        return hocon
-
-    def update(self, hocon):
-        if isinstance(hocon, pyhocon.config_tree.ConfigTree):
-            for name in hocon:
-                if name in HOCONArguments.RESERVED_NAME:
-                    raise ValueError(f'Invalid hocon attribute name(\'{name}\')')
-                if name not in self.__names:
-                    self.__names.append(name)
-                if isinstance(hocon[name], pyhocon.config_tree.ConfigTree):
-                    if hasattr(self, name):
-                        getattr(self, name).update(hocon[name])
-                    else:
-                        setattr(self, name, HOCONArguments(hocon[name]))
-                else:
-                    setattr(self, name, hocon[name])
-        else:
-            raise ValueError(f'Argument hocon(\'{hocon}\') is not a \'pyhocon.config_tree.ConfigTree\' object.')
-
-    def save(self, output_path, output_type='hocon'):
-        output_string = pyhocon.converter.HOCONConverter.convert(self.hocon, output_type)
-        with open(output_path, 'w', encoding='utf-8') as output_file:
-            output_file.write(output_string)
+from yoolkit.constant import Constant
+from yoolkit.arguments import load_arguments, update_arguments
 
 
 constant = Constant()
-constant.DEFAULT_HOCON_DIR = os.path.abspath(os.path.dirname(__file__))
-constant.DEFAULT_HOCON_PATH = os.path.join(constant.DEFAULT_HOCON_DIR, 'default.hocon')
-constant.DEFAULT_HOCON = pyhocon.ConfigFactory.parse_file(constant.DEFAULT_HOCON_PATH)
-
-
-def load_hocon(hocon_abs_path):
-    return pyhocon.ConfigFactory.parse_file(hocon_abs_path)
-
-
-def get_default_arguments():
-    default_arguments = HOCONArguments(constant.DEFAULT_HOCON)
-    return default_arguments
-
-
-def get_user_arguments(user_hocon=None):
-    default_arguments = get_default_arguments()
-    if user_hocon is not None:
-        default_arguments.update(user_hocon)
-    user_arguments = default_arguments
-    return user_arguments
+constant.DEFAULT_ARGUMENTS_DIR = os.path.abspath(os.path.dirname(__file__))
+constant.DEFAULT_ARGUMENTS_PATH = os.path.join(constant.DEFAULT_ARGUMENTS_DIR, 'default.hocon')
+constant.DEFAULT_ARGUMENTS = load_arguments(constant.DEFAULT_ARGUMENTS_PATH)
 
 
 def get_command_line_argument_parser():
@@ -151,11 +67,10 @@ def get_arguments():
     config_type = command_line_arguments.config_type
     if os.path.isfile(config_load_path):
         print('User configuration found -> Using user configuration.')
-        user_hocon = load_hocon(config_load_path)
-        user_arguments = get_user_arguments(user_hocon)
+        user_arguments = update_arguments(constant.DEFAULT_ARGUMENTS, config_load_path)
     else:
         print('No user configuration found -> Using default configuration.')
-        user_arguments = get_user_arguments()
+        user_arguments = constant.DEFAULT_ARGUMENTS
 
     if config_save_path == '':
         print('Configuration saving path not specified ! Saving no configuration.')
