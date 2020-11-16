9662c8ebbd28df04e7b252e53d2c00d58b5ddbf8
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-11-16 17:03:58 +0000

    Update ynmt command line interface

--- ynmt/binaries/main.py
+++ ynmt/binaries/main.py
@@ -10,11 +10,55 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import ynmt.hocon.arguments as harg
+import os
+import argparse
+from ynmt.hocon.arguments import constant, load_arguments
+
+
+def get_command_line_argument_parser():
+    argument_parser = argparse.ArgumentParser(allow_abbrev=True, formatter_class=argparse.RawTextHelpFormatter)
+    argument_parser.add_argument(
+        '-s',
+        '--config-save-path',
+        metavar='PATH',
+        default='',
+        help=('The path of configuration file to be saved,\n'
+              'configuration file will not be saved if set to \'\'.\n'
+              'DEFAULT=\'\''),
+    )
+    argument_parser.add_argument(
+        '-t',
+        '--config-type',
+        metavar='TYPE',
+        default='hocon',
+        choices=['hocon', 'json', 'yaml', 'properties'],
+        help=('The type of configuration file to be saved.\n'
+              'Choices=[\'hocon\', \'json\', \'yaml\', \'properties\']\n'
+              'DEFAULT=\'hocon\''),
+    )
+    return argument_parser
 
 
 def main():
-    args = harg.get_arguments()
+    command_line_argument_parser = get_command_line_argument_parser()
+    command_line_arguments = command_line_argument_parser.parse_args()
+
+    config_save_path = command_line_arguments.config_save_path
+    config_type = command_line_arguments.config_type
+
+    if config_save_path != '':
+        arguments = load_arguments(constant.ALL_SYSTEM_COMPONENTS_ARGUMENTS_PATH)
+        arguments.save(config_save_path, output_type=config_type)
+        print(f'Saving configuration file: {config_save_path}, type={config_type}')
+    else:
+        print(f'                >   Welcome to use YoungNMT!   <                ')
+        print(f'----------------------------------------------------------------')
+        print()
+        print(f'Please use the following command to make the most of the system:')
+        print(f'1. ynmt-preprocess --help')
+        print(f'2. ynmt-train --help')
+        print(f'3. ynmt-test --help')
+        print()
 
 
 if __name__ == '__main__':
