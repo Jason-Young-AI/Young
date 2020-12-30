9e44f15c9b75eecae36dc03a81b9ab11a8d0b956
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-12-30 07:37:13 +0000

    Fix bug of remove method & add __repr__ method

--- ynmt/data/dataset.py
+++ ynmt/data/dataset.py
@@ -10,26 +10,16 @@
 # LICENSE file in the root directory of this source tree.
 
 
+from ynmt.data.instance import Instance
+
+
 class Dataset(object):
     def __init__(self, structure):
         assert isinstance(structure, set), 'Type of structure should be set().'
         for attribute_name in structure:
-            assert isinstance(attribute_name, str), 'Type of {attribute_name} in structure should be str().'
+            assert isinstance(attribute_name, str), f'Type of {attribute_name} in structure should be str().'
         self.__structure = structure
         self.__instances = list()
-
-    def __getitem__(self, index):
-        return self.instances[index]
-
-    def __contains__(self, attribute_name):
-        return attribute_name in self.structure
-
-    def __iter__(self):
-        for instance in self.instances:
-            yield instance
-
-    def __len__(self):
-        return len(self.instances)
 
     @property
     def structure(self):
@@ -39,10 +29,26 @@ class Dataset(object):
     def instances(self):
         return self.__instances
 
+    def __len__(self):
+        return len(self.__instances)
+
+    def __iter__(self):
+        for instance in self.__instances:
+            yield instance
+
+    def __repr__(self):
+        return f'Dataset({self.structure!r}) # Maybe there are Instance(s) in Dataset!'
+
+    def __getitem__(self, index):
+        return self.__instances[index]
+
     def add(self, instance):
+        assert isinstance(instance, Instance), f'Type of instance should be Instance().'
         if instance.structure != self.structure:
-            raise ValueError('The structure of instance and dataset do not match!')
+            raise ValueError(f'The structure of instance and dataset does not match: {instance.structure} and {self.structure}!')
+
         self.__instances.append(instance)
 
     def remove(self, index):
-        del self.__instances[index]
+        assert index < len(self), f'Index out of range.'
+        self.__instances.pop(index)
