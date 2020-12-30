46b6b16987dfc166087d71f32f34c9d779193218
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-12-30 07:38:01 +0000

    Update Batch __init__ method & add __repr__ method & remove useless method

--- ynmt/data/batch.py
+++ ynmt/data/batch.py
@@ -10,40 +10,53 @@
 # LICENSE file in the root directory of this source tree.
 
 
+from ynmt.data.instance import Instance
+
+
 class Batch(object):
-    def __init__(self, structure, instances=list()):
+    def __init__(self, structure):
         assert isinstance(structure, set), 'Type of structure should be set().'
         for attribute_name in structure:
-            assert isinstance(attribute_name, str), 'Type of {attribute_name} in structure should be str().'
+            assert isinstance(attribute_name, str), f'Type of {attribute_name} in structure should be str().'
         self.__structure = structure
-
+        self.__size = 0
         for attribute_name in self.structure:
-            setattr(self, attribute_name, list())
+            self[attribute_name] = list()
 
-        for instance in instances:
-            for attribute_name in instance.structure:
-                if attribute_name in self.structure:
-                    attribute_value = getattr(self, attribute_name)
-                    attribute_value.append(instance[attribute_name])
+    @property
+    def structure(self):
+        return self.__structure
 
     def __len__(self):
-        return len(self.__structure)
-
-    def __setitem__(self, attribute_name, attribute_value):
-        if attribute_name not in self.__structure:
-            self.__structure.add(attribute_name)
-        self.__dict__[attribute_name] = attribute_value
-
-    def __getitem__(self, attribute_name):
-        return self.__dict__[attribute_name]
-
-    def __contains__(self, attribute_name):
-        return attribute_name in self.structure
+        return self.__size
 
     def __iter__(self):
         for attribute_name in self.structure:
             yield (attribute_name, self[attribute_name])
 
-    @property
-    def structure(self):
-        return self.__structure
+    def __repr__(self):
+        return f'Batch({self.structure!r}) # Maybe there are Instance(s) in Batch!'
+
+    def __setitem__(self, attribute_name, attribute_value):
+        assert attribute_name in self.__structure, f'Attribute name is not defined, only {self.structure} are allowed .'
+        self.__dict__[attribute_name] = attribute_value
+
+    def __getitem__(self, attribute_name):
+        assert attribute_name in self.__structure, f'Attribute name is not defined, only {self.structure} are allowed .'
+        return self.__dict__[attribute_name]
+
+    def add(self, instance):
+        assert isinstance(instance, Instance), f'Type of instance should be Instance().'
+        if instance.structure != self.structure:
+            raise ValueError(f'The structure of instance and batch does not match: {instance.structure} and {self.structure}!')
+
+        self.__size += 1
+        for attribute_name, attribute_value in instance:
+            self[attribute_name].append(attribute_value)
+
+    def remove(self, index):
+        assert index < len(self), f'Index out of range.'
+
+        self.__size -= 1
+        for attribute_name in self.structure:
+            self[attribute_name].pop(index)
